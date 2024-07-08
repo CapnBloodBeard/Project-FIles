672d553a4b1146c8423daedb2ac1023edba28137
CapnBloodBeard <77095391+CapnBloodBeard@users.noreply.github.com>
2024-07-08 13:11:59 +0530

    Where does this go

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        
+        int x=123;
+        System.out.println("Hello, World!"+x);
+
+        String greet = "hello";
+
+        System.out.println("Number is "+ greet);
+
+        String ok = "hmm";
+        String ko = "mmmh";
+
+        Scanner scnr = new Scanner(System.in);
+
+
+        ko=scnr.nextLine();
+    }
+
+
+}
+
